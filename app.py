f6f54e48c7771679f6c5f01cb85d9d19b015e208
Nisarg Chaudhari <nisrg5601@gmail.com>
2022-04-25 02:02:55 +0530

    basic api routes

--- app.py
+++ app.py
@@ -1,105 +1,113 @@
-from flask import Flask, jsonify, request
-from uuid import uuid4
 from blockchain import Blockchain
+from flask import Flask, jsonify, request
 
-# Instantiate the Node
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
-# Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', '')
-
-# Instantiate the Blockchain
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
-def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    proof = blockchain.proof_of_work(last_block)
-
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
-
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+@app.route('/', methods=['GET'])
+def home():
+    resp_json = {
+        'message': 'API is running!!' 
     }
-    return jsonify(response), 200
-
-
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    values = request.get_json()
-
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
-
-    # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonify(response), 201
-
+    return jsonify(resp_json), 200
 
 @app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
+def chain():
+    resp_json = {
+        'length' : len(blockchain.chain),
+        'chain' : blockchain.chain,
     }
-    return jsonify(response), 200
 
+    return jsonify(resp_json), 200
 
-@app.route('/nodes/register', methods=['POST'])
-def register_nodes():
-    values = request.get_json()
-
-    nodes = values.get('nodes')
-    if nodes is None:
-        return "Error: Please supply a valid list of nodes", 400
-
-    for node in nodes:
-        blockchain.register_node(node)
-
-    response = {
-        'message': 'New nodes have been added',
-        'total_nodes': list(blockchain.nodes),
+@app.route('/chain/state', methods=['GET'])
+def chain_state():
+    blockchain.update_state()
+    resp_json = {
+        'state': blockchain.state 
     }
-    return jsonify(response), 201
+    return jsonify(resp_json), 200
 
+@app.route('/funds', methods=['GET'])
+def funds():
+    resp_json = {
+        'message': ''
+    }
 
-@app.route('/nodes/resolve', methods=['GET'])
-def consensus():
-    replaced = blockchain.resolve_conflicts()
-
-    if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': blockchain.chain
-        }
+    id = request.args.get('id')
+    if id is None:
+        resp_json['message'] = "no 'id' in req params"
+        return resp_json, 200
     else:
-        response = {
-            'message': 'Our chain is authoritative',
-            'chain': blockchain.chain
-        }
+        resp_json['message'] = f'given id: {id}'
+        return resp_json, 200
 
-    return jsonify(response), 200
+@app.route('/funds/new', methods=['POST'])
+def new_funds():
+    resp_json = {
+        'message': 'init msg'
+    }
 
+    values = request.get_json()
+    title = values.get('title')
+    description = values.get('description')
+    if title is None or description is None:
+        resp_json['message'] = "Error: req body needs 'title' and 'description' values"
+        return resp_json, 400
+
+    if (not isinstance(title, str)) or title.isnumeric() or title=="":
+        resp_json['message'] = "Error: title must be a non-numeric non-empty string"
+        return resp_json, 400
+    if (not isinstance(description, str)) or description.isnumeric():
+        resp_json['message'] = "Error: description must be a non-numeric string"
+        return resp_json, 400
+
+    resp_json = {
+        'title' : title,
+        'description' : description
+    }
+    return resp_json, 200
+
+@app.route('/donations', methods=['GET'])
+def donations():
+    resp_json = {
+        'message': ''
+    }
+
+    fund_id = request.args.get('fund-id')
+    if fund_id is None:
+        resp_json['message'] = "Error: no 'fund-id' in req params"
+        return resp_json, 400
+    else:
+        resp_json['message'] = f'given fund-id: {fund_id}'
+        return resp_json, 200
+
+@app.route('/donations/new', methods=['POST'])
+def new_donations():
+    resp_json = {
+        'message': ''
+    }
+
+    fund_id = request.args.get('fund-id')
+    if fund_id is None:
+        resp_json['message'] = "Error: no 'fund-id' in req params"
+        return resp_json, 400
+    else:
+        values = request.get_json()
+        amount = values.get('amount')
+        if amount is None:
+            resp_json['message'] = "Error: req body needs 'amount'"
+            return resp_json, 400
+        else:
+            if (not isinstance(amount, (int, float))) or amount <= 0:
+                resp_json['message'] = "Error: amount must be a positive number"
+                return resp_json, 400
+            else:
+                resp_json['message'] = f"fund-id: {fund_id} | donation-amount: {amount}"
+                return resp_json, 200
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -109,4 +117,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port, debug=True)
