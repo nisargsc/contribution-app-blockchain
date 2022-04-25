d0337c92562d0cf9cacf48787313a30a68f8d043
Nisarg Chaudhari <nisrg5601@gmail.com>
2022-04-26 04:17:55 +0530

    flask_cors added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from blockchain import Blockchain
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 
 blockchain = Blockchain()
@@ -39,7 +41,7 @@ def funds():
     id = request.args.get('id')
     if id is None:
         resp_json = state.dict()
-        return resp_json, 200
+        return jsonify(resp_json), 200
     else:
         fund = state.funds[id]
         resp_json = {
@@ -49,7 +51,7 @@ def funds():
                 'donations' : fund.donations
             }
         }
-        return resp_json, 200
+        return jsonify(resp_json), 200
 
 @app.route('/funds/new', methods=['POST'])
 def new_funds():
@@ -62,14 +64,14 @@ def new_funds():
     description = values.get('description')
     if title is None or description is None:
         resp_json['message'] = "Error: req body needs 'title' and 'description' values"
-        return resp_json, 400
+        return jsonify(resp_json), 400
 
     if (not isinstance(title, str)) or title.isnumeric() or title=="":
         resp_json['message'] = "Error: title must be a non-numeric non-empty string"
-        return resp_json, 400
+        return jsonify(resp_json), 400
     if (not isinstance(description, str)) or description.isnumeric():
         resp_json['message'] = "Error: description must be a non-numeric string"
-        return resp_json, 400
+        return jsonify(resp_json), 400
     
     blockchain.mine_block_add_fund(title,description)
     state = blockchain.get_state()
@@ -78,7 +80,7 @@ def new_funds():
         'message' : 'new block mined, fund added, and state updated',
         'state' : state.dict()
     }
-    return resp_json, 200
+    return jsonify(resp_json), 200
 
 @app.route('/donations', methods=['GET'])
 def donations():
@@ -91,13 +93,13 @@ def donations():
     fund_id = request.args.get('fund_id')
     if fund_id is None:
         resp_json['message'] = "Error: no 'fund_id' in req params"
-        return resp_json, 400
+        return jsonify(resp_json), 400
     else:
         fund = state.funds[fund_id]
         resp_json = {
             'donations' : fund.donations
         }
-        return resp_json, 200
+        return jsonify(resp_json), 200
 
 @app.route('/donations/new', methods=['POST'])
 def new_donations():
@@ -108,17 +110,17 @@ def new_donations():
     fund_id = request.args.get('fund_id')
     if fund_id is None:
         resp_json['message'] = "Error: no 'fund_id' in req params"
-        return resp_json, 400
+        return jsonify(resp_json), 400
     else:
         values = request.get_json()
         amount = values.get('amount')
         if amount is None:
             resp_json['message'] = "Error: req body needs 'amount'"
-            return resp_json, 400
+            return jsonify(resp_json), 400
         else:
             if (not isinstance(amount, (int, float))) or amount <= 0:
                 resp_json['message'] = "Error: amount must be a positive number"
-                return resp_json, 400
+                return jsonify(resp_json), 400
             else:
                 blockchain.mine_block_add_donation(amount, fund_id)
                 state = blockchain.get_state()
@@ -128,7 +130,7 @@ def new_donations():
                     'message' : f'new block mined, new donation added to fund {fund_id}',
                     'donations' : fund.donations
                 }
-                return resp_json, 200
+                return jsonify(resp_json), 200
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
